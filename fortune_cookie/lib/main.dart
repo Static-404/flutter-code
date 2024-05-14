8f84bf6f89a9ab346ef54bacec2d7764f040d379
Static-404 <pboybams16@gmail.com>
2024-05-14 19:54:44 +0100

    fortune cookie

---
+++ fortune_cookie/lib/main.dart
@@ -0,0 +1,100 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 124, 89, 184)),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({super.key});
+
+  String _currentFortune = "";
+
+  // This widget is the home page of your application. It is stateful, meaning.
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _currentFortune = "";
+
+  final _fortuneList = [
+    "you will receive favour tomorrow",
+    "you are blessed",
+    "you are highly favoured",
+    "call me",
+    "your name is praise",
+    "Do have a nice day"
+  ];
+
+  void _randomFortune() {
+    var random = Random();
+    int fortune = random.nextInt(_fortuneList.length);
+    setState(() {
+      _currentFortune = _fortuneList[fortune];
+    });
+    print(_currentFortune);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(150, 73, 73, 30),
+        title: Text("Fortune Cookie"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // Text(
+            //   "Your fortunne is:",
+            //   style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+            // ),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  //'${_fortuneList[_counter % _fortuneList.length]}',
+                  "${_currentFortune}",
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            ElevatedButton(
+                onPressed: _randomFortune, child: Text('Get Fortune'))
+          ],
+        ),
+      ),
+      //floatingActionButton: FloatingActionButton(
+      //onPressed: _randomFortune,
+      //tooltip: 'Increment',
+      //child: const Icon(Icons.add),
+      //), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
